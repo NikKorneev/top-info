abc344be2443aac708b81ee1f3998bea3eed1813
Nick <nikita_korneev101@mail.ru>
2025-06-23 13:21:02 +0300

    fix blockquote text color

--- components/animated/accordionContent.tsx
+++ components/animated/accordionContent.tsx
@@ -98,7 +98,7 @@ const AccordionContent = ({ description, id, image, title }: Props) => {
 									style={{
 										borderLeft: "4px solid #ccc",
 										paddingLeft: "1rem",
-										color: "#555",
+										color: "white",
 										fontStyle: "italic",
 										margin: "1em 0",
 									}}
